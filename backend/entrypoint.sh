3c380e9c3813e918aa230a9ef3c7168b38e79460
Daria Gelfman <daria.gelfman@gmail.com>
2023-08-10 20:12:36 +0300

    fix requeriments, entrypoint, isort, dockerfile

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+set -e
 
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 cp -r /app/collected_static/. /static/static/
-gunicorn --bind 0.0.0.0:9000 kittygram_backend.wsgi
+
+exec "$@"
